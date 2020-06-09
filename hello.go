a179439645d9e8e1fdce27b27396748caf4876b8
Christina  Ng <cmatson93@gmail.com>
2020-06-08 21:58:54 -0700

    hello tests complete

--- hello.go
+++ hello.go
@@ -2,11 +2,35 @@ package main
 
 import "fmt"
 
+const spanish = "Spanish"
+const french = "French"
+const englishHelloPrefix = "Hello, "
+const spanishHelloPrefix = "Hola, "
+const frenchHelloPrefix = "Bonjor, "
+
 //Hello is a function that returns Hello World string
-func Hello(name string) string {
-	return "Hello, " + name
+func Hello(name string, language string) string {
+	if name == "" {
+		name = "World"
+	}
+
+	return greetingPrefix(language) + name
+}
+
+func greetingPrefix(language string) (prefix string) {
+
+	switch language {
+	case french:
+		prefix = frenchHelloPrefix
+	case spanish:
+		prefix = spanishHelloPrefix
+	default:
+		prefix = englishHelloPrefix
+	}
+
+	return
 }
 
 func main() {
-	fmt.Println(Hello("world"))
+	fmt.Println(Hello("world", ""))
 }
--- hello_test.go
+++ hello_test.go
@@ -3,10 +3,35 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	got := Hello("Christina")
-	want := "Hello, Christina"
 
-	if got != want {
-		t.Errorf("got %q want %q", got, want)
+	assertCorrectMessage := func(t *testing.T, got, want string) {
+		t.Helper()
+		if got != want {
+			t.Errorf("got %q want %q", got, want)
+		}
 	}
+
+	t.Run("saying hello to people", func(t *testing.T) {
+		got := Hello("Christina", "")
+		want := "Hello, Christina"
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("say 'Hello, World' when an empty string is supplied", func(t *testing.T) {
+		got := Hello("", "")
+		want := "Hello, World"
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("say 'Hola, Sonia' when spanish is suplied as a language", func(t *testing.T) {
+		got := Hello("Sonia", "Spanish")
+		want := "Hola, Sonia"
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("say 'Bonjor, Antoni' when french is suplied as a language", func(t *testing.T) {
+		got := Hello("Antoni", "French")
+		want := "Bonjor, Antoni"
+		assertCorrectMessage(t, got, want)
+	})
 }
